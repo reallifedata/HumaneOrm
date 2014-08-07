a316fbbd8bede817550f623deb7633667ec3e934
Peter Weissbrod <talktopete@gmail.com>
2014-08-06 21:57:11 -0400

    code drop

---
+++ HumaneOrm/Program.fs
@@ -0,0 +1,25 @@
+﻿namespace HumaneOrm
+open System
+open Sql
+open Lexer
+module Program =
+    //open Sql
+    [<EntryPoint>]
+        let main argv = 
+            let x = "   
+                SELECT x, y, z   
+                FROM t1   
+                LEFT JOIN t2   
+                INNER JOIN t3 ON t3.ID = t2.ID   
+                WHERE x = 50 AND y = 20   
+                ORDER BY x ASC, y DESC, z   
+            "   
+ 
+            let lexbuf = LexBuffer<char>.FromString text
+            //let y = SqlParser.start SqlLexer.tokenize lexbuf   
+            //printfn "%A" y   
+ 
+            Console.WriteLine("(press any key)")   
+            Console.ReadKey(true) |> ignore
+            0
+
