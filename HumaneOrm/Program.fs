13b4a594a5fcf7eeb8c6c8ccf987acb7744cc054
Peter Weissbrod <talktopete@gmail.com>
2014-08-08 17:41:57 -0400

    added fully functional stripSpExecuteSql

--- HumaneOrm/Program.fs
+++ HumaneOrm/Program.fs
@@ -7,28 +7,7 @@ open Lexer
 open Microsoft.FSharp.Text.Lexing
 
 module Program =
-    let (|Match|_|) pattern input =
-        let re = new Regex(pattern)
-        let m = re.Match(input) in
-        if m.Success then Some ((re.GetGroupNames()
-                                    |> Seq.map (fun n -> (n, m.Groups.[n]))
-                                    |> Seq.filter (fun (n, g) -> g.Success)
-                                    |> Seq.map (fun (n, g) -> (n, g.Value))
-                                    |> Map.ofSeq)) else None
-
-
-    let stripSpExecuteSql (input:string) =
-        if not(input.Trim().StartsWith("exec sp_executesql")) then 
-            input
-        else
-            if not(input.Contains("@p__linq__0")) then
-                input.Trim().Replace("exec sp_executesql N'","").TrimEnd([|'\''|])
-            else
-                let back = input.Trim().Replace("exec sp_executesql N'","")
-
-    [<EntryPoint>]
-        let main argv = 
-            let x = "   
+    let test01 = "   
                 SELECT x, y, z   
                 FROM t1   
                 LEFT JOIN t2   
@@ -36,7 +15,7 @@ module Program =
                 WHERE x = 50 AND y = 20  AND z='bar'
                 ORDER BY x ASC, y DESC, z   
             "   
-            let x1 = "
+    let test02 = "
             exec sp_executesql N'SELECT 
 [GroupBy1].[A1] AS [C1]
 FROM ( SELECT 
@@ -45,11 +24,27 @@ FROM ( SELECT
     WHERE ([Extent1].[Column] LIKE @p__linq__0 ESCAPE N''~'') AND ([Extent1].[Column] LIKE @p__linq__1 ESCAPE N''~'')
 )  AS [GroupBy1]',N'@p__linq__0 nvarchar(4000),@p__linq__1 nvarchar(4000)',@p__linq__0=N'%text1%',@p__linq__1=N'%text2%'
             "
-            printfn "%A" (stripSpExecuteSql x1)
-            let lexbuf = LexBuffer<_>.FromString x
-            let y = Parser.start Lexer.tokenize lexbuf   
-            printfn "%A" y   
- 
+
+    let stripSpExecuteSql (input:string) =
+        if not(input.Trim().StartsWith("exec sp_executesql")) then 
+            input.Trim()
+        else
+            if not(input.Contains("@p__linq__0")) then
+                input.Trim().Replace("exec sp_executesql N'","").TrimEnd([|'\''|])
+            else
+                let stripped = input.Trim().Replace("exec sp_executesql N'","") |> (fun x -> x.Substring(0, x.IndexOf("]',")+1))
+                (new Regex(@"@p__linq__[\d]+=[^,]+")).Matches(input) 
+                    |> Seq.cast
+                    |> Seq.map(fun x-> x:> System.Text.RegularExpressions.Match)
+                    |> Seq.map(fun x-> ((x.Value.Split [|'='|]).[0].Trim(), (x.Value.Split [|'='|]).[1].Trim()))
+                    //|> Seq.iter(fun x-> printfn "%A" x)
+                    |> Seq.fold(fun (acc:string) (k, v) -> acc.Replace(k,v)) stripped
+    [<EntryPoint>]
+        let main argv = 
+            let sql = stripSpExecuteSql test02
+            printfn "Before: %A" sql
+            //let lexbuf = LexBuffer<_>.FromString sql
+            //Parser.start Lexer.tokenize lexbuf |> printfn "%A" 
             Console.WriteLine("(press any key)")   
             Console.ReadKey(true) |> ignore
             0
